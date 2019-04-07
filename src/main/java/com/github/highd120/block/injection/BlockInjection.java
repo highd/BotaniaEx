ae90182e2af423b916eae7afeecf5914e844d42b
hdgames123 <hdgames123@yahoo.co.jp>
2019-04-07 18:54:17 +0900

    InjcetionBlockのBoundingBoxを修正。

--- src/main/java/com/github/highd120/block/injection/BlockInjection.java
+++ src/main/java/com/github/highd120/block/injection/BlockInjection.java
@@ -1,5 +1,7 @@
 package com.github.highd120.block.injection;
 
+import javax.annotation.Nonnull;
+
 import com.github.highd120.Lexicon;
 import com.github.highd120.block.BlockStand;
 import com.github.highd120.util.block.BlockRegister;
@@ -9,7 +11,9 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import vazkii.botania.api.lexicon.ILexiconable;
 import vazkii.botania.api.lexicon.LexiconEntry;
@@ -18,9 +22,17 @@ import vazkii.botania.api.wand.IWandable;
 @BlockRegister(name = "injection")
 public class BlockInjection extends BlockStand implements IWandable, ILexiconable {
 
+    private static final AxisAlignedBB AABB = new AxisAlignedBB(0, 0, 0, 1, 1, 1);
+
     @Override
     public boolean isFullCube(IBlockState state) {
         return true;
+    }
+
+    @Nonnull
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess world, BlockPos pos) {
+        return AABB;
     }
 
     @Override
